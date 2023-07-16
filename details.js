3cba626899ade8a6005e55288a5d4c74caefc7be
Divyanshu Kumar <4674kumardivyanshu@gmail.com>
2023-07-16 15:53:39 +0530

    index renamed

--- details.js
+++ details.js
@@ -3,21 +3,25 @@ document.getElementById("details-form").addEventListener("submit", function(even
   
     // Retrieve values from the form
     var name = document.getElementById("name").value;
-    var points = parseInt(document.getElementById("points").value);
+    const points = parseInt(document.getElementById("points").value);
   
     // Save the values to local storage
     localStorage.setItem("name", name);
     localStorage.setItem("points", points);
   
     // Redirect to the slot machine page
-    window.location.href = "slot_machine.html";
+    verifyAge();
   });
+
+
+
   function verifyAge() {
     var age = prompt("Are you 18 years or older? (Yes/No)").toLowerCase();
     
     if (age === "yes") {
-      window.location.href = "slot_machine.html";
-    } else {
+    window.location.href = "slot_machine.html";
+    } 
+    else {
       alert("You must be 18 or older to play the Slot Machine.");
     }
   }
