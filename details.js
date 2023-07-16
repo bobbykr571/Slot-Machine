c244ce7ef8e1ecb0beba41d6e44f46c7e0097287
Divyanshu Kumar <4674kumardivyanshu@gmail.com>
2023-07-16 16:58:30 +0530

    bug fixed

--- details.js
+++ details.js
@@ -1,27 +1,25 @@
-document.getElementById("details-form").addEventListener("submit", function(event) {
-    event.preventDefault(); // Prevent form submission
-  
-    // Retrieve values from the form
-    var name = document.getElementById("name").value;
-    const points = parseInt(document.getElementById("points").value);
-  
-    // Save the values to local storage
-    localStorage.setItem("name", name);
-    localStorage.setItem("points", points);
-  
-    // Redirect to the slot machine page
-    verifyAge();
-  });
+function verifyAge() {
+  var age = prompt("Are you 18 years or older? (Yes/No)").toLowerCase();
 
-
-
-  function verifyAge() {
-    var age = prompt("Are you 18 years or older? (Yes/No)").toLowerCase();
-    
-    if (age === "yes") {
-    window.location.href = "slot_machine.html";
-    } 
-    else {
-      alert("You must be 18 or older to play the Slot Machine.");
-    }
+  if (age === "yes") {
+    return "slot_machine.html";
+  } else {
+    return "not_allowed.html";
   }
+}
+
+// console.log(typeof verifyAge()); // "function"
+
+const detailsForm = document.querySelector("#details-form");
+
+if (detailsForm) {
+  detailsForm.addEventListener("submit", function(event) {
+    event.preventDefault(); // Prevent form submission
+
+    // Call the verifyAge() function
+    var redirectUrl = verifyAge();
+
+    // Redirect to the appropriate page
+    window.location.href = redirectUrl;
+  });
+}
